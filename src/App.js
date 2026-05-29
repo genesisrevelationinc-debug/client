4c38569e1bcc894bfc67549af2d6cb2b3b27cc37
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-28 20:26:11 -0400

    fix: apply solution for issue #13

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import WelcomeModal from './components/WelcomeModal';
+
+function App() {
+  return (
+    <div className="App">
+
+export default App;
+
+function WelcomeModal() {
+  return (
+    <WelcomeModal />
+  );
+}
